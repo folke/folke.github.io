78a59bc7988ffbc463a949897c598d6303d04b14
Folke Lemaitre <folke@engagor.com>
2015-10-15 17:48:12 -0700

    embody cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,7 +1,6 @@
 
-hexo.extend.tag.register('card', function(args, content){
+hexo.extend.tag.register('card', function(args, content) {
     var url = args[0];
     content = content || url;
-
-  return '<a class="embedly-card" href="' + url + '">' + content + '</a>';
+    return '<a class="embedly-card" href="' + url + '">' + content + '</a>';
 }, {ends: true});
